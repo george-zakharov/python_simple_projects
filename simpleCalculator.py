fbbe26f46a58aa0c16fc7a43109a6d7939cbd883
George Zakharov <george_zakharov@mail.ru>
2018-11-11 20:39:15 +0300

    Add scientific operations

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,48 +1,78 @@
 #!/usr/bin/env python3
 
-import sys, math
+import sys
+import math
 
 
-def basic():
-    print('Basic calculator is activated')
-    source_data = {
-        'first_number': float(input('Type in first number: ')),
-        'operation': str(input('Type in operation sign: ')),
-        'second_number': float(input('Type in second number: ')),
-    }
+def calculator_handler(calculator_type):
+    result = ''
 
-    if source_data['operation'] not in ['+', '-', '*', '/']:
-        print('Operation sign must be one of: + - * /')
+    if calculator_type == 1:
+        print('Basic calculator is activated')
+        source_data = {
+            'first_number': float(input('Type in first number: ')),
+            'operation': str(input('Type in operation sign: ')).strip(),
+            'second_number': float(input('Type in second number: ')),
+        }
 
-    result = do_basic_calculations(source_data)
+        if source_data['operation'] not in ['+', '-', '*', '/']:
+            sys.exit('Operation sign must be one of: + - * /')
+
+        result = do_calculations(source_data, calculator_type)
+    elif calculator_type == 2:
+        print('Scientific calculator is activated')
+        source_data = {
+            'operation': str(input('Type in operation: ')),
+            'number': float(input('Type in number: ')),
+        }
+
+        if source_data['operation'] not in ['ceil', 'exp', 'log10', 'log2', 'sqrt', 'cos', 'sin', 'pi', 'e']:
+            sys.exit('Operation sign must be one of: ceil, exp, log10, log2, sqrt, cos, sin, pi, e')
+
+        result = do_calculations(source_data, calculator_type)
 
     print('Result is:')
     print(result)
 
 
-def do_basic_calculations(data):
+def do_calculations(data, calculator_type):
     result = 0
-    if data['operation'] is '+':
-        result = data['first_number'] + data['second_number']
-    elif data['operation'] is '-':
-        result = data['first_number'] - data['second_number']
-    elif data['operation'] is '*':
-        result = data['first_number'] * data['second_number']
-    elif data['operation'] is '/':
-        result = data['first_number'] / data['second_number']
+
+    if calculator_type == 1:
+        if data['operation'] is '+':
+            result = data['first_number'] + data['second_number']
+        elif data['operation'] is '-':
+            result = data['first_number'] - data['second_number']
+        elif data['operation'] is '*':
+            result = data['first_number'] * data['second_number']
+        elif data['operation'] is '/':
+            result = data['first_number'] / data['second_number']
+    elif calculator_type == 2:
+        if data['operation'] == 'ceil':
+            result = math.ceil(data['number'])
+        elif data['operation'] == 'exp':
+            result = math.exp(data['number'])
+        elif data['operation'] == 'log10':
+            result = math.log10(data['number'])
+        elif data['operation'] == 'log2':
+            result = math.log2(data['number'])
+        elif data['operation'] == 'sqrt':
+            result = math.sqrt(data['number'])
+        elif data['operation'] == 'cos':
+            result = math.cos(data['number'])
+        elif data['operation'] == 'sin':
+            result = math.sin(data['number'])
+        elif data['operation'] == 'pi':
+            result = math.pi
+        elif data['operation'] == 'e':
+            result = math.e
 
     return result
 
 
-def scientific():
-    print('Scientific calculator is activated')
-
-
 print('Select calculator type:\n1.Basic\n2.Scietific\n')
 calculator_type = int(input('Enter calculator number: '))
-if calculator_type == 1:
-    basic()
-elif calculator_type == 2:
-    scientific()
+if calculator_type in [1, 2]:
+    calculator_handler(calculator_type)
 else:
-    sys.exit('You should enter valid number of calculator type, please!')
+    sys.exit('Enter valid number of calculator type, please!')
